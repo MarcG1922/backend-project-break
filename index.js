da6cf5d7dd5b6ef7115b79c8a81d4515af30e898
Marc Granero <magrba@floridauniversitaria.es>
2026-02-16 19:00:21 +0100

    Día 1: servidor y MongoDB funcionando

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const methodOverride = require('method-override');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Servidor funcionando 🚀</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🔥 Servidor corriendo en puerto ${PORT}`);
+});
